3f324df25dbf603a284b03c4d8d568980f34fbab
rafaelnmo <rafaelnevesmello@gmail.com>
2022-04-11 23:32:09 -0300

    Updating to use Mongo DB Atlas

--- login-system-backend/config/config.js
+++ login-system-backend/config/config.js
@@ -1,4 +1,4 @@
-const config = {
+/*const config = {
 	env: process.env.NODE_ENV || "development",
 	port: process.env.PORT || 3000,
 	jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
@@ -13,3 +13,35 @@ const config = {
 };
 
 export default config;
+*/
+
+// Password: k2swm4p6
+// MyfirstDatabase: mernproject
+//mongodb+srv://roliveira:<password>@cluster0.em6ub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const password = "K2swm4p6g8h9";
+
+const config = {
+	env: process.env.NODE_ENV || "development",
+	port: process.env.PORT || 3000,
+	jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+	mongoUri:
+		"mongodb+srv://roliveira:" +
+		password +
+		"@cluster0.em6ub.mongodb.net/mernproject?retryWrites=true&w=majority",
+};
+
+export default config;
+
+/*
+
+const config = {
+	env: process.env.NODE_ENV || "development",
+	port: process.env.PORT || 3000,
+	jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+	mongoUri:
+		"mongodb+srv://roliveira:K2swm4p6g8h9@cluster0.em6ub.mongodb.net/mernproject?retryWrites=true&w=majority",
+};
+
+export default config;
+*/
